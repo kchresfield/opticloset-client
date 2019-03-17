21e4f5854eb64e70ebc1553ef94ab56474b04c15
lauraannpena <lauraannpena@gmail.com>
2019-03-16 20:45:55 -0500

    imported apiservice to tab 5

--- client/src/app/tab5/tab5.page.ts
+++ client/src/app/tab5/tab5.page.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api/api.service';
 
 @Component({
   selector: 'app-tab5',
   templateUrl: 'tab5.page.html',
   styleUrls: ['tab5.page.scss']
 })
-export class Tab5Page { }
+export class Tab5Page implements OnInit { 
+  closet: any;
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getCloset(data => {
+      console.log(data);
+      this.closet = data;
+    });
+  }
+}
