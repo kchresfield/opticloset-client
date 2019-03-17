06d6312b2ab239d236a34c331f050b04259d5688
lauraannpena <lauraannpena@gmail.com>
2019-03-17 18:40:46 -0500

    ootd renders with random outfit and save button

--- client/src/app/tab1/tab1.page.ts
+++ client/src/app/tab1/tab1.page.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit  } from '@angular/core';
-import { AuthService } from '../services/auth/auth.service';
+import { ApiService } from '../services/api/api.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  providers: [AuthService],
+  providers: [ApiService],
 })
+
 export class Tab1Page implements OnInit {
+  closet: any;
+  top: any; // 1 or 2
+  onePiece: any; // 3
+  outerwear: any; // 4
+  accessory: any; // 5
+  bottom: any; // 6
+  shoes: any; // 13
 
-  constructor(private authService: AuthService) {
-
+  constructor(private apiService: ApiService) {
   }
-
+  
   ngOnInit() {
-    this.authService.getProfile((err, profile) => {
-      console.log(profile);
-    })
+    this.apiService.getCloset(clothes => {
+      this.top = [...clothes].filter((clothing) => clothing.id_category === 1 || clothing.id_category === 2);
+      this.onePiece = [...clothes].filter((clothing) => clothing.id_category === 3);
+      this.outerwear = [...clothes].filter((clothing) => clothing.id_category === 4);
+      this.accessory = [...clothes].filter((clothing) => clothing.id_category === 5);
+      this.bottom = [...clothes].filter((clothing) => clothing.id_category === 6);
+      this.shoes = [...clothes].filter((clothing) => clothing.id_category === 13);
+      // console.log(this.tops, this.onePiece, this.outerwear, this.accessory, this.bottom);
+    });
   }
+
+  getRandomIndex = (max) => {
+    const maxInt = Math.floor(max);
+    return Math.floor(Math.random() * (maxInt));
+  };
 
 }
