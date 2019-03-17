6d68e7a294122b5f0dcf161529d9e3b4f9bbabea
lauraannpena <lauraannpena@gmail.com>
2019-03-17 00:52:44 -0500

    add sort method radio buttons to choose sort method

--- client/src/app/tab5/tab5.page.ts
+++ client/src/app/tab5/tab5.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api/api.service';
+import { ClothingItem } from '../clothing-item';
 
 @Component({
   selector: 'app-tab5',
@@ -8,15 +9,31 @@ import { ApiService } from '../services/api/api.service';
 })
 export class Tab5Page implements OnInit { 
   closet: any;
+  sort: any;
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
     this.apiService.getCloset(clothes => {
-      this.closet = clothes;
+      this.closet = clothes.sort((a, b) => {
+        return a.count_worn - b.count_worn;
+      });
       this.closet.forEach(clothing => {
         clothing.lastUpdated = new Date(clothing.updatedAt).toString().slice(3, 15);
       })
       console.log(this.closet)
     });
+  }
+
+  sortCloset(method) {
+    if (method === 'most2Least') {
+      this.closet.sort((a, b) => {
+        return b.count_worn - a.count_worn;
+      })
+    }
+    if (method === 'least2Most') {
+      this.closet.sort((a, b) => {
+        return a.count_worn - b.count_worn;
+      })
+    }
   }
 }
