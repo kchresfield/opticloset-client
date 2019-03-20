48ddc3da238127306f69379eb563d4acfbf6ce84
Julien de la Mettrie <jdelamettrie@gmail.com>
2019-03-20 14:55:33 -0500

    manually copied Laura's latest PR with fix for tab1 => but Tab1 doesn't render properly => Cannot read property 'imageUrl' of undefined

--- client/src/app/tab1/tab1.page.ts
+++ client/src/app/tab1/tab1.page.ts
@@ -1,48 +1,68 @@
 import { Component, OnInit  } from '@angular/core';
 import { ApiService } from '../services/api/api.service';
 import { ToastController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  providers: [ApiService, ToastController],
+  providers: [ApiService, ToastController]
 })
-
 export class Tab1Page implements OnInit {
-  closet: any;
-  top: any; // 1 or 2
-  onePiece: any; // 3
-  outerwear: any; // 4
-  accessory: any; // 5
-  bottom: any; // 6
-  shoe: any; // 13
+  closet: Array<Object>;
+  top: Object; // 1 or 2
+  onePiece: Object; // 3
+  outerwear: Object; // 4
+  accessory: Object; // 5
+  bottom: Object; // 6
+  shoe: Object; // 13
+  isLoading: Boolean;
+  outfitSelected: Boolean;
 
-  constructor(private apiService: ApiService, public toastController: ToastController) {
-  }
-  
+  constructor(
+    private apiService: ApiService,
+    public loadingController: LoadingController, 
+    public toastController: ToastController
+  ) {}
+
   ngOnInit() {
+    this.isLoading = true;
+    this.outfitSelected = false;
+    this.presentLoadingWithOptions();
     this.apiService.getCloset(clothes => {
       this.closet = clothes;
       this.chooseOutfit();
+      this.isLoading = false;
     });
   }
-  
+
   async presentToast() {
     const toast = await this.toastController.create({
       message: 'Great Choice!',
       position: 'middle',
       animated: true,
-      duration: 3000,
+      duration: 3000
     });
     toast.present();
   }
-  
-  getRandomIndex = (max) => {
+
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      spinner: null,
+      duration: 3000,
+      message: 'One moment while we select your outfit...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    return await loading.present();
+  }
+
+  getRandomIndex = max => {
     const maxInt = Math.floor(max);
-    return Math.floor(Math.random() * (maxInt));
+    return Math.floor(Math.random() * maxInt);
   };
-  
+
   chooseOutfit = () => {
     // copy closet and filter by category
     const tops = [...this.closet].filter((clothing) => clothing.id_category === 1 || clothing.id_category === 2);
@@ -58,7 +78,5 @@ export class Tab1Page implements OnInit {
     this.accessory = accessories[this.getRandomIndex(accessories.length)];
     this.bottom = bottoms[this.getRandomIndex(bottoms.length)];
     this.shoe = shoes[this.getRandomIndex(shoes.length)];
-    // console.log(this.tops, this.onePiece, this.outerwear, this.accessory, this.bottom);
   };
-
 }
