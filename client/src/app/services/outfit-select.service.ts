33e1a240d0c1aab4af653b0f08d3eb1e8ec1b480
lauraannpena <lauraannpena@gmail.com>
2019-03-22 15:29:45 -0500

    MVP monochromatic algo functionality

--- client/src/app/services/outfit-select.service.ts
+++ client/src/app/services/outfit-select.service.ts
@@ -1,10 +1,18 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
+import { mockCloset } from './mockClosetData.js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OutfitSelectService {
   outfit: any;
+  closet: any;
+  tops: any;
+  bottoms: any;
+  outerwears: any;
+  onePieces: any;
+  accessories: any;
+  shoes: any;
   constructor() {}
 
 
@@ -19,6 +27,16 @@ export class OutfitSelectService {
   set(category, item) {
     this.outfit[category] = item;
     console.log(this);
+  }
+
+  setMock() {
+    this.closet = mockCloset;
+    this.tops = [...this.closet].filter((clothing) => clothing['id_category'] === 1 || clothing['id_category'] === 2);
+    this.onePieces = [...this.closet].filter((clothing) => clothing['id_category'] === 3);
+    this.outerwears = [...this.closet].filter((clothing) => clothing['id_category'] === 4);
+    this.accessories = [...this.closet].filter((clothing) => clothing['id_category'] === 5);
+    this.bottoms = [...this.closet].filter((clothing) => clothing['id_category'] === 6);
+    this.shoes = [...this.closet].filter((clothing) => clothing['id_category'] === 13);
   }
 
   // return the current outfit of the day
@@ -37,11 +55,40 @@ export class OutfitSelectService {
     return 'colormatch'
   }
 
-  monochromatic(arrayOfClothingObjs) {
-    return 'mono'
+  monochromatic() {
+    // object to hold current outfit being built
+    const monoOutfit = {};
+    // start with random top
+    const starterpiece = this.tops[this.getRandomIndex(this.tops.length)];
+    // get color from top - NOTE TO LAURA - AMEND LATER TO CHOOSE EITHER FIRST OR SECOND COLOR
+    const color = starterpiece.color.split(', ')[0];
+    // assign top to current outfit
+    monoOutfit['top'] = starterpiece;
+
+    // loop through bottoms to select matching bottom by color
+    for (let i = 0; i < this.bottoms.length; i++) {
+      // check if current bottom matches top by color
+      if(this.bottoms[i].color.includes(color)) {
+        monoOutfit['bottom'] = this.bottoms[i];
+      }
+    }
+
+    // loop through outwears to select matching outerwears by color
+    for (let i = 0; i < this.outerwears.length; i++) {
+      // check if current outerwear matches top by color
+      if (this.outerwears[i].color.includes(color)) {
+        monoOutfit['outerwear'] = this.outerwears[i];
+      }
+    }
+
+    monoOutfit['shoes'] = this.shoes[this.getRandomIndex(this.shoes.length)]
+    monoOutfit['accessory'] = this.accessories[this.getRandomIndex(this.accessories.length)]
+
+    return monoOutfit;
   }
 
-  allNeutral(arrayOfClothingObjs) {
+  allNeutral() {
+    const neutrals = ['black', 'grey', 'white', 'tan', 'navy']
     return 'neutrals'
   }
 
@@ -49,19 +96,47 @@ export class OutfitSelectService {
     //remember to import weather apiservice for weather
   }
 
-  checkOccasion() {
-
+  //checks to see if each clothing item in outfit is the same occasion
+  //returns a boolean
+  checkOccasion(outfitObj) {
+    // const occasion = outfitObj['top']['occasion'];
+    // for (let key in outfitObj) {
+    //   if (outfitObj[key]['occasion'] !== occasion) {
+    //     return false;
+    //   }
+    // }
+    return true;
   }
 
+  // selects random match method
   chooseMatchMethod() {
     const methods = ['colorMatch', 'monochromatic', 'allNeutral'];
     const randomMethod = methods[this.getRandomIndex(methods.length)];
     return randomMethod;
   }
 
-  // colormatching algo
+  // select matching outfit
+  // takes in match method and reassigns OOTD to outfit with that method
+  // order of selection: occasion => weather => matching
   chooseMatchingOutfit(method) {
-    //choose color method
+    // if no method is selected, select random matching method
+    if (!method) {
+      method = this.chooseMatchMethod();
+    }
 
+    // current outfit selected by method
+    const currOutfitSelection = this[method](this.closet);
+    
+    // check if clothing selections are all the same occasion
+    // NOTE FOR LAURA - MAYBE PUT THIS CONDITIONAL IN WHEN THE OUTFIT IS ACTUALLY BUILDING AND NOT WHEN IT'S FINISHED
+    if (this.checkOccasion(currOutfitSelection)) {
+      // reassign outfit to be outfit chosen by method
+      this.outfit = currOutfitSelection;
+      console.log(this.outfit, 'outfit selected');
+      return;
+    } else {
+      console.log('recurse');
+      // this.chooseMatchingOutfit(method, count + 1, totalCombos);
+    }
   }
 }
