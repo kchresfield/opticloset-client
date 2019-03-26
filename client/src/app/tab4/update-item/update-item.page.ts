4e800f403fcf1128abb672a727712c9276f85c56
Julien de la Mettrie <jdelamettrie@gmail.com>
2019-03-26 16:09:54 -0500

    update item feature fixed

--- client/src/app/tab4/update-item/update-item.page.ts
+++ client/src/app/tab4/update-item/update-item.page.ts
@@ -121,18 +121,55 @@ export class UpdateItemPage implements OnInit {
     this.setItem();
   }
 
+  setItem() {
+    // set the selected item on update-item to be the same as the item on the outfitSelectService
+    this.selectedItem = this.outfitSelectService.get('selectedItem');
+
+    // set the selectedColors to be the ones from the selected item by spliting the colors string
+    this.selectedItem.color.split(', ').forEach(color => {
+      this.selectedColors.push(`${color[0].toUpperCase()}${color.slice(1)}`); // then pushing into selectedColors
+    });
+
+    // set the attributes button colors to primary
+    this.selectedItem.attribute.split(', ').forEach((attribute) => {
+      // debugger;
+      if (Object.keys(this.buttonColors).indexOf(attribute) !== -1) {
+        this.buttonColors[attribute] = 'primary';
+      }
+    });
+    // set the occasion button color to primary
+    this.buttonColors[occasions[this.selectedItem.id_occasion]] = 'primary';
+
+    // set the selectedOccasionID to be the one from the selectedItem
+    this.selectedOccasionID = this.selectedItem.id_occasion;
+
+    // set the selectedCategory to be the one from the selectedItem
+    this.selectedCategory.push(
+      `${this.selectedItem.category[0].toUpperCase()}${this.selectedItem.category.slice(1)}`
+    );
+    this.selectedCategoryID = this.selectedItem.id_category;
+
+    // set the initial price based on the selectedItem
+    this.initialprice = this.selectedItem.price;
+
+  }
+
   updateItem() {
+    // gather all the attributes and occasions with a color set as primary
     Object.keys(this.buttonColors).forEach(button => {
-      if (this.buttonColors[button] === 'primary') {
-        if (this.allAttributes.hasOwnProperty(button)) {
-          this.selectedAttributes.push(button);
+      if (this.buttonColors[button] === 'primary') { // for all buttons with color 'primary'
+        if (this.allAttributes.hasOwnProperty(button)) { // check if the button is an attribute
+          this.selectedAttributes.push(button); // if so => push the button into the selectedAttributes array
         }
-        if (this.allOccasions.hasOwnProperty(button)) {
-          this.selectedOccasions.push(button);
+        if (this.allOccasions.hasOwnProperty(button)) { // check if button is an occasion
+          this.selectedOccasions.push(button); // if so => push the button into the selectedOccasions array
         }
       }
     });
 
+    // if (!this.price) {
+    //   this.price = this.initialprice;
+    // }
     // create the updated item to be input in the DB
     this.updatedItem = {
       id_clothing_item: this.selectedItem.id_clothing_item,
@@ -142,7 +179,8 @@ export class UpdateItemPage implements OnInit {
       count_worn: this.selectedItem.count_worn,
       id_occasion: this.selectedOccasionID,
       attribute: this.selectedAttributes.join(', '),
-      color: this.selectedColors.join(', ')
+      color: this.selectedColors.join(', '),
+      category: this.selectedCategory[0],
     };
     this.apiService.updateClothingItem(this.updatedItem).subscribe((result) => {
       console.log(result);
@@ -157,23 +195,8 @@ export class UpdateItemPage implements OnInit {
   print() {
     console.log(this);
   }
-  setItem() {
-    this.selectedItem = this.outfitSelectService.get('selectedItem');
-    this.selectedItem.color.split(', ').forEach(color => {
-      this.selectedColors.push(`${color[0].toUpperCase()}${color.slice(1)}`);
-    });
-    this.selectedItem.attribute.split(', ').forEach((attribute) => {
-      if (Object.keys(this.buttonColors).indexOf(attribute) !== 1) {
-        this.buttonColors[attribute] = 'primary';
-      }
-    });
-    this.buttonColors[occasions[this.selectedItem.id_occasion]] = 'primary';
-    this.selectedCategory.push(
-      `${this.selectedItem.category[0].toUpperCase()}${this.selectedItem.category.slice(1)}`
-    );
-    this.initialprice = this.selectedItem.price;
 
-  }
+
 
   attributeItem(input) {
     if (this.allAttributes[input] === true) {
@@ -194,9 +217,11 @@ export class UpdateItemPage implements OnInit {
 
   selectCategory(input) {
     console.log(input);
+    // debugger;
     this.selectedCategory.splice(0, 2, `${category[input][0].toUpperCase()}${category[input].slice(1)}`);
     this.selectedCategoryID = input;
   }
+
 
   colorUserClicked(input) {
     console.log(input);
@@ -205,6 +230,7 @@ export class UpdateItemPage implements OnInit {
     }
   }
 
+  // present toast based on the server's response
   async presentToast(message) {
     const toast = await this.toastController.create({
       message: message,
